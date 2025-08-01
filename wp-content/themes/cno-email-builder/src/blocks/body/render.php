96b8e761742803bfc9c7b36ce6e30d9eed3ac795
KJ Roelke <kroelke@choctawnation.com>
2025-08-01 15:05:57 -0500

    update theme and add better email support

--- wp-content/themes/cno-email-builder/src/blocks/body/render.php
+++ wp-content/themes/cno-email-builder/src/blocks/body/render.php
@@ -6,14 +6,13 @@
  */
 
 $preview_text = isset( $block->context['cno-email-blocks/previewText'] ) ? $block->context['cno-email-blocks/previewText'] : null;
-?>
 
-<body <?php echo get_block_wrapper_attributes(); ?>>
-	<?php if ( ! empty( $preview_text ) ) : ?>
-		<div style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0" data-skip-in-text="true">
-			<?php echo $preview_text; ?>
-		</div>
-		<?php
-endif;
-	echo $content;
-	?>
+echo '<body ' . get_block_wrapper_attributes( array( 'style' => 'margin:0;padding:0;-webkit-text-size-adjust:none' ) ) . '>';
+?>
+<?php if ( ! empty( $preview_text ) ) : ?>
+	<div style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0" data-skip-in-text="true">
+		<?php echo $preview_text; ?>
+	</div>
+<?php endif; ?>
+<?php
+echo $content;
