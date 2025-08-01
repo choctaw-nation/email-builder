68daf0492c27c3921b9d548391eea80a908c8fa3
KJ Roelke <kroelke@choctawnation.com>
2025-08-01 13:46:52 -0500

    add divider block

--- wp-content/themes/cno-email-builder/src/blocks/divider/index.tsx
+++ wp-content/themes/cno-email-builder/src/blocks/divider/index.tsx
@@ -1,17 +1,101 @@
 import { registerBlockType } from '@wordpress/blocks';
+import {
+	useBlockProps,
+	InspectorControls,
+	HeightControl,
+} from '@wordpress/block-editor';
+import { BorderControl, Panel, PanelBody } from '@wordpress/components';
 
-import Edit from './edit';
-import save from './save';
 import metadata from './block.json';
+import { separator } from '@wordpress/icons';
 
 registerBlockType( metadata.name, {
+	icon: separator,
 	/**
 	 * @see ./edit.js
 	 */
-	edit: Edit,
+	edit: ( { attributes, setAttributes } ) => {
+		return (
+			<>
+				<InspectorControls>
+					<Panel>
+						<PanelBody title="Divider Settings">
+							<div
+								style={ {
+									display: 'flex',
+									flexDirection: 'column',
+									gap: '10px',
+								} }
+							>
+								<HeightControl
+									label="Width"
+									value={ attributes.width || '' }
+									onChange={ ( value ) => {
+										setAttributes( {
+											width:
+												'' === value ? '100%' : value,
+										} );
+									} }
+								/>
+								<BorderControl
+									__next40pxDefaultSize
+									enableStyle
+									label="Border"
+									value={ attributes.border || {} }
+									onChange={ ( val ) => {
+										console.log( val );
+										setAttributes( {
+											border: val,
+										} );
+									} }
+									shouldSanitizeBorder
+								/>
+							</div>
+						</PanelBody>
+					</Panel>
+				</InspectorControls>
+				<hr
+					{ ...useBlockProps( {
+						style: getBlockStyle( attributes ),
+					} ) }
+				/>
+			</>
+		);
+	},
 
 	/**
 	 * @see ./save.js
 	 */
-	save,
+	save: ( { attributes } ) => (
+		<hr
+			{ ...useBlockProps.save( { style: getBlockStyle( attributes ) } ) }
+		/>
+	),
 } );
+
+function getBlockStyle( attributes ): React.CSSProperties {
+	const { border, width } = attributes;
+	const blockStyle: React.CSSProperties = {};
+	if ( Object.values( border ).some( ( val ) => val ) ) {
+		blockStyle.border = 0;
+		if ( border?.color ) {
+			blockStyle.borderColor = border.color;
+		}
+
+		if ( border?.width ) {
+			blockStyle.borderTopWidth = border.width;
+		}
+
+		if ( border?.style ) {
+			blockStyle.borderStyle = border.style;
+		}
+	}
+
+	if ( width ) {
+		blockStyle.marginLeft = 'auto';
+		blockStyle.marginRight = 'auto';
+		blockStyle.width = width;
+	}
+
+	return blockStyle;
+}
