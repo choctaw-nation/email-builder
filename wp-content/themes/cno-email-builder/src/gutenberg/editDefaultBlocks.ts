b5ee5ca3d25ac4226908f712eb47ff2e1e192f22
KJ Roelke <kroelke@choctawnation.com>
2025-08-01 12:37:51 -0500

    update theme

--- wp-content/themes/cno-email-builder/src/gutenberg/editDefaultBlocks.ts
+++ wp-content/themes/cno-email-builder/src/gutenberg/editDefaultBlocks.ts
@@ -1,11 +1,13 @@
 import { addFilter } from '@wordpress/hooks';
 import domReady from '@wordpress/dom-ready';
 import removeDefaultBlockClasses from './removeDefaultBlockClasses';
+import { disableAdvancedPanel } from './disableAdvancedPanel';
 
 domReady( () => {
 	const namespace = 'cno-email-builder';
 	const filters = {
 		'cno-remove-default-block-classes': removeDefaultBlockClasses,
+		'cno-disable-advanced-panel': disableAdvancedPanel,
 	};
 	Object.entries( filters ).forEach( ( [ filterName, filterCallback ] ) => {
 		addFilter(
