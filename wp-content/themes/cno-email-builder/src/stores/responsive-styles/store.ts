a9fff7bfbd21d7a3517a6da5cbf862e23de286d3
KJ Roelke <kroelke@choctawnation.com>
2025-08-04 09:21:37 -0500

    finish store migration

--- wp-content/themes/cno-email-builder/src/stores/responsive-styles/store.ts
+++ wp-content/themes/cno-email-builder/src/stores/responsive-styles/store.ts
@@ -1,37 +1,11 @@
 import { createReduxStore, register } from '@wordpress/data';
 import { STORES } from '../consts';
-import { State, ResponsiveStyleAction } from './types';
 import { actions } from './actions';
 import { selectors } from './selectors';
-
-const initialState: State = {
-	col: [],
-	image: [],
-	divider: [],
-};
+import reducer from './reducer';
 
 const store = createReduxStore( STORES.RESPONSIVE_STYLES, {
-	reducer( state: State = initialState, action: ResponsiveStyleAction ) {
-		switch ( action.type ) {
-			case 'ADD_BLOCK_TYPE':
-				return {
-					...state,
-					[ action.blockType ]: [
-						...( state[ action.blockType ] || [] ),
-						action.blockId,
-					],
-				};
-			case 'REMOVE_BLOCK_TYPE':
-				return {
-					...state,
-					[ action.blockType ]: (
-						state[ action.blockType ] || []
-					).filter( ( id ) => id !== action.blockId ),
-				};
-			default:
-				return state;
-		}
-	},
+	reducer,
 	actions,
 	selectors,
 } );
