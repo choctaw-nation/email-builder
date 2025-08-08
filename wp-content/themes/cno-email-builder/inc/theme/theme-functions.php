60705bd26db1d239f8fcdf5717d866156ef82b5e
KJ Roelke <kroelke@choctawnation.com>
2025-08-08 11:35:22 -0500

    redo 1.0.1 with simplified commit history
    * update phpcs gha (#7)
    
    * 1.0.1 (#8)
    
    * add bootstrap
    
    * add basic layout
    
    * refactor js from php into its own ts file
    
    * update theme version and init readme + changelog
    
    * clean up code
    
    * fix package-lock (#9)
    
    * bump packages version number (#10)
    
    * switch deploy action version
    
    * fix lock file sync for ci
    
    * revert build step back to main

--- wp-content/themes/cno-email-builder/inc/theme/theme-functions.php
+++ wp-content/themes/cno-email-builder/inc/theme/theme-functions.php
@@ -53,3 +53,25 @@ function cno_read_svg( string $logo_path, string|false $alt_text, string $fallba
 function cno_echo_svg( string $logo_path, string|false $alt_text, string $fallback = 'This file could not be found' ): void {
 	echo cno_read_svg( $logo_path, $alt_text, $fallback );
 }
+
+/**
+ * Gets the content of the current post as JSON.
+ *
+ * @param bool $strip_comments Whether to strip HTML comments from the content. Default `true`.
+ * @return string|null The JSON-encoded content or null if not available.
+ */
+function cno_get_email_content( bool $strip_comments = true ): ?string {
+	if ( empty( get_the_content() ) ) {
+		return null;
+	}
+	if ( ! is_user_logged_in() || ! is_singular() ) {
+		return null;
+	}
+	$content = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' . get_the_content();
+
+	if ( $strip_comments ) {
+		$content = preg_replace( '/<!--[\s\S]*?-->/', '', $content );
+	}
+
+	return $content;
+}
