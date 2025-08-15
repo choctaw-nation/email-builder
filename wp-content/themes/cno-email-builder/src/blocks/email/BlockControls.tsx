a84cbd2e6f6ad84fc66caff933b4f27105135386
KJ Roelke <kroelke@choctawnation.com>
2025-08-15 08:55:34 -0500

    update block name and migrate email bg to email block settings

--- wp-content/themes/cno-email-builder/src/blocks/email/BlockControls.tsx
+++ wp-content/themes/cno-email-builder/src/blocks/email/BlockControls.tsx
@@ -1,5 +1,6 @@
 import { InspectorControls } from '@wordpress/block-editor';
 import {
+	ColorPalette,
 	Panel,
 	PanelBody,
 	PanelRow,
@@ -12,17 +13,25 @@ import './editor.scss';
 
 import FontControl from './font-controls/_FontControl';
 import { DEFAULT_FONT_URL } from './font-controls/utils';
+import useColorPalettes from '../_shared/hooks/useColorPalettes';
 
 export default function BlockControls( props ) {
 	const {
-		attributes: { title, previewText, useDefaultFonts, fontUrl },
+		attributes: {
+			title,
+			previewText,
+			useDefaultFonts,
+			fontUrl,
+			backgroundColor,
+		},
 		setAttributes,
 	} = props;
+	const { choctawLanding, baseColorsPalette } = useColorPalettes();
 
 	return (
 		<InspectorControls>
-			<Panel header="Email Settings">
-				<PanelBody title="Meta Information">
+			<Panel header="Meta Email Settings">
+				<PanelBody title="Preview Settings">
 					<PanelRow>
 						<TextControl
 							__next40pxDefaultSize
@@ -46,6 +55,23 @@ export default function BlockControls( props ) {
 						/>
 					</PanelRow>
 				</PanelBody>
+				<PanelBody title="Preview Background Color">
+					<ColorPalette
+						__experimentalIsRenderedInSidebar
+						title="Background Color"
+						value={ backgroundColor }
+						colors={ [ baseColorsPalette, choctawLanding ] }
+						onChange={ ( backgroundColor ) =>
+							setAttributes( { backgroundColor } )
+						}
+						headingLevel={ 3 }
+					/>
+				</PanelBody>
+				<PanelBody title="Color Definitions" initialOpen={ false }>
+					<div>Color definitions can go here</div>
+				</PanelBody>
+			</Panel>
+			<Panel header="Fonts">
 				<PanelBody title="Font Import Location">
 					<ToggleGroupControl
 						__next40pxDefaultSize
@@ -86,9 +112,6 @@ export default function BlockControls( props ) {
 					) }
 				</PanelBody>
 				<FontControl { ...props } />
-				<PanelBody title="Color Definitions" initialOpen={ false }>
-					<div>Color definitions can go here</div>
-				</PanelBody>
 			</Panel>
 		</InspectorControls>
 	);
