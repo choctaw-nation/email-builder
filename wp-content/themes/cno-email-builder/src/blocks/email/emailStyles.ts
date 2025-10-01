433d5120fedf32d5fbf4eb09f611df1b96cc27ed
KJ Roelke <kroelke@choctawnation.com>
2025-10-01 09:11:22 -0500

    Merge pull request #39 from choctaw-nation/1.3.1
    update font wrapping

--- wp-content/themes/cno-email-builder/src/blocks/email/emailStyles.ts
+++ wp-content/themes/cno-email-builder/src/blocks/email/emailStyles.ts
@@ -3,12 +3,12 @@ import type { FontsData } from '../_lib/types';
 export default function emailStyles( headingsFont: FontsData, bodyFont: FontsData ): string {
 	return `
 		body,p,a,td {
-			text-wrap:balance;
+			text-wrap:pretty;
 			font-family: ${ bodyFont.name }, ${ bodyFont.fallbackStack.value }
 		}
 		
 		h1,h2,h3,h4,h5,h6 {
-			text-wrap:balance;
+			text-wrap:pretty;
 			font-family: ${ headingsFont.name }, ${ headingsFont.fallbackStack.value };
 		}
 	`;
