40691a8e322398732c660bb1dc8beef6d1784e30
KJ Roelke <kroelke@choctawnation.com>
2025-10-01 13:34:22 -0500

    add button block

--- wp-content/themes/cno-email-builder/src/blocks/button/save.tsx
+++ wp-content/themes/cno-email-builder/src/blocks/button/save.tsx
@@ -1,9 +1,30 @@
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { calcStyleObject } from '../_shared/TypographyControls';
+import { calcSpacingObject } from '../_shared/SpacingControl';
 
-export default function save() {
-	return (
-		<p { ...useBlockProps.save() }>
-			{ 'Container – hello from the saved content!' }
-		</p>
-	);
+export default function save( { attributes } ) {
+	const { content, linkDestination, rel, linkTarget, backgroundColor, borderColor, borderWidth, borderRadius } = attributes;
+	const blockProps = useBlockProps.save( {
+		style: {
+			...calcStyleObject( attributes ),
+			...calcSpacingObject( attributes ),
+			display: 'inline-block',
+			textDecoration: 'none',
+			textAlign: attributes.textAlign || 'left',
+			borderColor,
+			backgroundColor,
+			borderRadius,
+			borderWidth,
+			borderStyle: 'solid',
+		},
+		align: attributes.textAlign || 'left',
+	} );
+	return <RichText.Content
+		{ ...blockProps }
+		tagName="a"
+		value={ content }
+		href={ linkDestination }
+		rel={ rel }
+		target={ linkTarget }
+	/>;
 }
