1a969443c66f288ec4169249de433c9efbc8a75d
KJ Roelke <kroelke@choctawnation.com>
2025-11-19 13:13:05 -0600

    fix responsive css-rules

--- wp-content/themes/cno-email-builder/src/blocks/email/index.tsx
+++ wp-content/themes/cno-email-builder/src/blocks/email/index.tsx
@@ -55,7 +55,7 @@ registerBlockType( metadata.name, {
 					}");:where(.email-wrapper__body) {${ emailStyles(
 						attributes.headingsFont,
 						attributes.bodyFont
-					) }}@media screen and (max-width:450px) {.responsive-col {width: 100% !important;display:block!important;padding:0!important;}.responsive-col.not-last {margin-bottom:10px;}}.responsive-col img {max-width:450px!important;width:100%!important;}`,
+					) }}@media screen and (max-width:450px) {.responsive-col {width: 100% !important;display:block!important;padding:0!important;}.responsive-col.not-last {margin-bottom:10px;}.responsive-col img {max-width:450px!important;width:100%!important;}}`,
 						} }
 					/>
 				) }
